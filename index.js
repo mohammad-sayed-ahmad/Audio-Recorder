1c6c4daec0d1957d8183a572ec5d9fd7acb0732d
MSA <the1leader.mohammad@gmail.com>
2025-07-15 20:34:53 +0300

    Removed need for tab-content and tab-panes. Made line variable functional.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const importArea = document.getElementById("importArea");
-
 async function modifyImportArea(event) {
     const importFileText = await event.target.files[0].text();
     const trimmedText = importFileText.trim();
     
-    importArea.textContent = trimmedText;
+    document.getElementById("importArea").textContent = trimmedText;
+}
+
+function generateLines(text) {
+    return text.split('\n').map((x) => {
+        return {"text": x, "recording": undefined};
+    });
 }
 
 document.getElementById("importFile").addEventListener("input", modifyImportArea)
