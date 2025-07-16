b9265aba8e69cc7d56d5185340de67c40d69bdd8
MSA <the1leader.mohammad@gmail.com>
2025-07-16 21:22:08 +0300

    Removed bootstrap. Added static folder.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const textArea = document.getElementById("importArea");
 async function modifyImportArea(event) {
     const importFileText = await event.target.files[0].text();
     const trimmedText = importFileText.trim();
     
-    document.getElementById("importArea").textContent = trimmedText;
+    textArea.value = trimmedText;
+    textArea.dispatchEvent(new Event('input'));
+    
 }
 document.getElementById("importFile").addEventListener("input", modifyImportArea)
 
@@ -11,9 +14,8 @@ function generateLines(text) {
     for (const [index, line] of text.split('\n').entries()){
         newLines.push({
             text: line,
-            recording: null,
-            shown: index == 0
+            recording: null
         })
     }
+    return newLines;
 }
-
