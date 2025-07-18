6aec76b36fd83f5dcb070440954ab31320b59ff1
MSA <the1leader.mohammad@gmail.com>
2025-07-18 06:21:54 +0300

    Added microphone functionality with IndexedDB.

--- recording.js
+++ recording.js
@@ -8,6 +8,10 @@ function setUpRecording() {
         navigator.mediaDevices.getUserMedia({audio: true})
         .then((stream) => {
             const recorder = new MediaRecorder(stream);
+            recorder.onerror = () => {
+                console.error("Issue with recording.");
+            }
+            console.debug('MediaRecorder created:', recorder);
             resolve(recorder);
         })
         .catch((err) => {
@@ -33,45 +37,68 @@ function setUpDB() {
         const db = event.target.result;
         if (!db.objectStoreNames.contains('audioStore')) {
             db.createObjectStore('audioStore', {keyPath: "index"});
-            console.info("Created audioStore");
+            console.debug("Created audioStore");
         }
+    }
+
+    request.onerror = 
+    (event) => {
+        console.error("Issue with AudioDB");
     }
 
 }
 
-function storeAudioData(transaction, currentIndex){
+function storeAudioData(currentIndex){
     return (event) => {
-        var array = transaction.get(currentIndex).recording;
-        console.log('Stored Audio data');
-        array.push(event.data);
-        transaction.put({
-            index: currentIndex,
-            recording: array
-        })
+        const transaction = db.transaction(["audioStore"], "readwrite");
+        const objectStore = transaction.objectStore('audioStore');
+
+        const getRequest = objectStore.get(currentIndex);
+        getRequest.onsuccess = (e) => {
+            const data = e.target.result;
+            if (data) {
+                console.debug('Stored Audio data');
+                data.recording.push(event.data); // Push new blob to existing array
+                objectStore.put(data); // Put the updated data back
+            } else {
+                // This case handles if the initial put in startLineRecording somehow failed or was too slow
+                // Or if data for currentIndex doesn't exist yet, which shouldn't happen if startLineRecording ran.
+                console.warn(`No existing record found for index ${currentIndex}. Creating new entry.`);
+                objectStore.put({
+                    index: currentIndex,
+                    recording: [event.data]
+                });
+            }
+        };
+
     }
 }
 
 function startLineRecording(currentIndex){
-    const transaction = db.transaction(["audioStore"], "readwrite").objectStore('audioStore');
-    transaction.put({
+    mediaRecorder.requestData();
+    const objectStore = db.transaction(["audioStore"], "readwrite").objectStore('audioStore');
+    objectStore.put({
         index: currentIndex,
         recording: []
     });
-    mediaRecorder.ondataavailable = storeAudioData(transaction, currentIndex);
+    mediaRecorder.ondataavailable = storeAudioData(currentIndex);
+    mediaRecorder.requestData();
 }
 
-async function startRecording(currentIndex) {
+async function startRecording(currentIndex, isRecording) {
     if (!mediaRecorder){
         mediaRecorder = await setUpRecording();
     }
-    console.log(mediaRecorder);
-    startLineRecording(currentIndex);
     mediaRecorder.start();
+    isRecording = true;
+    console.debug('MediaRecorder state:', mediaRecorder.state);
+    startLineRecording(currentIndex);
 }
 
-function stopRecording(){
-    mediaRecorder.stream.getTracks().forEach(track => track.stop());
+function stopRecording(isRecording){
     mediaRecorder.stop();
+    mediaRecorder.stream.getAudioTracks()[0].stop();
+    mediaRecorder = null;
 }
 
 setUpDB();
